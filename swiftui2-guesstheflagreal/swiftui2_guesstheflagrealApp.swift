b68392019dcb4dfa8341380779ebda896e763535
amcsw <aldo.chocos@amcsoftware.pe>
2022-08-20 00:01:22 -0500

    Initial Commit

---
+++ swiftui2-guesstheflagreal/swiftui2_guesstheflagrealApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui2_guesstheflagrealApp.swift
+//  swiftui2-guesstheflagreal
+//
+//  Created by Aldo Chocos on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui2_guesstheflagrealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
